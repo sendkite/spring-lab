a26f007e92b5ef121f58ce2f31c14cf462afca63
sendkite <sendkite1@gmail.com>
2022-06-03 20:29:50 +0900

    [web server] init 설정

---
+++ web-applicarion-server-master/src/main/java/webserver/WebServer.java
@@ -0,0 +1,34 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    private static final int DEFAULT_PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        int port = 0;
+        if (args == null || args.length == 0) {
+            port = DEFAULT_PORT;
+        } else {
+            port = Integer.parseInt(args[0]);
+        }
+
+        // 서버 소켓 생성
+        try (ServerSocket listenSocket = new ServerSocket(port)) {
+            log.info("Web Application Server started {} port", port);
+
+            // 클라 연결 대기
+            Socket connection;
+            while ((connection = listenSocket.accept()) != null) {
+                RequestHandler requestHandler = new RequestHandler(connection);
+                requestHandler.start();
+            }
+        }
+    }
+}
